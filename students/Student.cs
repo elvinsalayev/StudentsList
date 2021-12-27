edcea5d689b94ebddaf12db02818a985d509b2d9
Elvin Salayev <94475641+elvinsalayev@users.noreply.github.com>
2021-12-27 17:48:18 +0400

    Update Student.cs

--- students/Student.cs
+++ students/Student.cs
@@ -12,14 +12,14 @@ namespace Generics.First
         public string Name { get; set; }
         public string Surname { get; set; }
         public int Age { get; set; }
-        public string Sex { get; set; }
+        public string Gender { get; set; }
         public int School { get; set; }
         public string Class { get; set; }
 
 
         public override string ToString()
         {
-            return $"{Name} {Surname} / {Age} / {Sex} / {School} / {Class}";
+            return $"{Name} {Surname} / {Age} / {Gender} / {School} / {Class}";
         }
     }
 }
